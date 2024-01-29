47e75357f964942cb9d0b8eab7c40efe04c67318
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-29 17:37:08 +0100

    Implemented DebitCard class as model of resources

--- DebitCardService/DebitCardService.DataAccess/Entities/DebitCard.cs
+++ DebitCardService/DebitCardService.DataAccess/Entities/DebitCard.cs
@@ -2,4 +2,19 @@
 
 public class DebitCard
 {
+    public int Id { get; set; }
+    public string? AccountNumber { get; set; }
+    public decimal Amount { get; set; }
+    public string? CardNumber { get; set; }
+    public DateOnly ExpirityDate { get; set; }
+    public int Cvv2Code { get; set; }
+    public int PinCode { get; set; }
+    public string? CardHolder { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsActiveCashWithdrawal { get; set; }
+    public int CashWithdrawalLimit { get; set; }
+    public bool IsActiveByPayPass { get; set; }
+    public int PayPassLimit { get; set; }
+    public bool IsActiveByPhone { get; set; }
+    public int PhoneLimit { get; set; }
 }
