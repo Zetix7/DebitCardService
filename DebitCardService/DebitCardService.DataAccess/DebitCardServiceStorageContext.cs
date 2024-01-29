5f5d252cbda4747332bec0753fbae7ceb6fce307
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-29 18:00:39 +0100

    Added EntityFramework to projects, created DebitCardServiceStorageContext and DebitCardServiceStorageContextFactory classes

---
+++ DebitCardService/DebitCardService.DataAccess/DebitCardServiceStorageContext.cs
@@ -0,0 +1,13 @@
+﻿using DebitCardService.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DebitCardService.DataAccess;
+
+public class DebitCardServiceStorageContext : DbContext
+{
+    public DebitCardServiceStorageContext(DbContextOptions<DebitCardServiceStorageContext> options) : base(options)
+    {
+    }
+
+    public DbSet<DebitCard> DebitCards { get; set; }
+}
---
+++ DebitCardService/DebitCardService.DataAccess/DebitCardServiceStorageContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DebitCardService.DataAccess;
+
+public class DebitCardServiceStorageContextFactory : IDesignTimeDbContextFactory<DebitCardServiceStorageContext>
+{
+    public DebitCardServiceStorageContext CreateDbContext(string[] args)
+    {
+        var optionBuilder = new DbContextOptionsBuilder<DebitCardServiceStorageContext>();
+        optionBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=DebitCardServiceStorage;Integrated Security=True;Encrypt=False");
+        return new DebitCardServiceStorageContext(optionBuilder.Options);
+    }
+}
