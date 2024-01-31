78e40fccd9e0346849ef56691bc9771a58a01ef1
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-31 17:31:04 +0100

    Created DebitCard class as domain model

---
+++ DebitCardService/DebitCardService.ApplicationServices/API/Domain/Models/DebitCard.cs
@@ -0,0 +1,11 @@
+﻿namespace DebitCardService.ApplicationServices.API.Domain.Models;
+
+public class DebitCard
+{
+    public string? AccountNumber { get; set; }
+    public decimal Amount { get; set; }
+    public string? CardNumber { get; set; }
+    public DateTime ExpirityDate { get; set; }
+    public string? CardHolder { get; set; }
+    public bool IsActive { get; set; }
+}
