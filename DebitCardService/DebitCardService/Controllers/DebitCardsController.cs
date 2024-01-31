78674d5ab3f564a29d6145d4db7a642258973dcd
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-31 17:27:51 +0100

    Created DebitCardsController class

---
+++ DebitCardService/DebitCardService/Controllers/DebitCardsController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DebitCardService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DebitCardsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public DebitCardsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+}
