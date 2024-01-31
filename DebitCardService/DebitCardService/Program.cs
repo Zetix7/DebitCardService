f6afea28f67ae298889b842a4f37e9e6f2b74237
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-31 14:43:57 +0100

    Added ConnectionStrings in appsettings.json file, added DebitCardServiceStorageContext class to services in Program class

--- DebitCardService/DebitCardService/Program.cs
+++ DebitCardService/DebitCardService/Program.cs
@@ -1,10 +1,13 @@
 using DebitCardService.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddDbContext<DebitCardServiceStorageContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DebitCardServiceDatabaseConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
