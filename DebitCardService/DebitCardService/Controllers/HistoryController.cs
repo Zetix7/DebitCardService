2b7b019014e66e38ee21d75063ea885cd8024049
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-31 18:18:14 +0100

    Created HistoryController class and added GetAllHistory endpoint

---
+++ DebitCardService/DebitCardService/Controllers/HistoryController.cs
@@ -0,0 +1,25 @@
+﻿using DebitCardService.ApplicationServices.API.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DebitCardService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HistoryController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public HistoryController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> GetAllHistory([FromQuery] GetAllHistoryRequest request)
+    {
+        var response = await _mediator.Send(request);
+        return Ok(response);
+    }
+}
