6080d2856918f618f51e005ebba7c205023cc615
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-01-31 18:08:41 +0100

    Created History class as domain model

---
+++ DebitCardService/DebitCardService.ApplicationServices/API/Domain/Models/History.cs
@@ -0,0 +1,12 @@
+﻿namespace DebitCardService.ApplicationServices.API.Domain.Models;
+
+public class History
+{
+    public DateTime DateOfOperation { get; set; } = DateTime.Now;
+    public string? Sender { get; set; }
+    public string? SenderAccountNumber { get; set; }
+    public string? Recipient { get; set; }
+    public string? RecipientAccountNumber { get; set; }
+    public decimal Amount { get; set; }
+    public string? Title { get; set; }
+}
