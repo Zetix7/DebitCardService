778336353fde9ec35b8d2638192d50ed324c9e84
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-02-02 19:27:10 +0100

    Created GetAllDebitCardsQuery class, modified GetAllDebitCardsHandler class

--- DebitCardService/DebitCardService.ApplicationServices/API/Handlers/GetAllDebitCardsHandler.cs
+++ DebitCardService/DebitCardService.ApplicationServices/API/Handlers/GetAllDebitCardsHandler.cs
@@ -1,25 +1,27 @@
 ﻿using AutoMapper;
 using DebitCardService.ApplicationServices.API.Domain;
 using DebitCardService.ApplicationServices.API.Domain.Models;
-using DebitCardService.DataAccess;
+using DebitCardService.DataAccess.CQRS;
+using DebitCardService.DataAccess.CQRS.Queries;
 using MediatR;
 
 namespace DebitCardService.ApplicationServices.API.Handlers;
 
 public class GetAllDebitCardsHandler : IRequestHandler<GetAllDebitCardsRequest, GetAllDebitCardsResponse>
 {
-    private readonly IRepository<DataAccess.Entities.DebitCard> _debitCardRepository;
     private readonly IMapper _mapper;
+    private readonly IQueryExecutor _queryExecutor;
 
-    public GetAllDebitCardsHandler(IRepository<DataAccess.Entities.DebitCard> debitCardRepository, IMapper mapper)
+    public GetAllDebitCardsHandler(IMapper mapper, IQueryExecutor queryExecutor)
     {
-        _debitCardRepository = debitCardRepository;
         _mapper = mapper;
+        _queryExecutor = queryExecutor;
     }
 
     public async Task<GetAllDebitCardsResponse> Handle(GetAllDebitCardsRequest request, CancellationToken cancellationToken)
     {
-        var debitCards = await _debitCardRepository.GetAll();
+        var query = new GetAllDebitCardsQuery();
+        var debitCards = await _queryExecutor.Execute(query);
         var mappedDebitCards = _mapper.Map<List<DebitCard>>(debitCards);
 
         var response = new GetAllDebitCardsResponse
---
+++ DebitCardService/DebitCardService.DataAccess/CQRS/Queries/GetAllDebitCardsQuery.cs
@@ -0,0 +1,12 @@
+﻿using DebitCardService.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DebitCardService.DataAccess.CQRS.Queries;
+
+public class GetAllDebitCardsQuery : QueryBase<List<DebitCard>>
+{
+    public override Task<List<DebitCard>> Execute(DebitCardServiceStorageContext context)
+    {
+        return context.DebitCards.ToListAsync();
+    }
+}
