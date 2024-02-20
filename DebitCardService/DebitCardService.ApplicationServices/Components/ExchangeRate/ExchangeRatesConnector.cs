1a0e79b2d3d03fb4854a66c42d7a7738af82792e
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-02-20 08:55:57 +0100

    Fix connection string

--- DebitCardService/DebitCardService.ApplicationServices/Components/ExchangeRate/ExchangeRatesConnector.cs
+++ DebitCardService/DebitCardService.ApplicationServices/Components/ExchangeRate/ExchangeRatesConnector.cs
@@ -13,13 +13,13 @@ public class ExchangeRatesConnector : IExchangeRatesConnector
     public ExchangeRatesConnector(IConfiguration configuration)
     {
         _configuration = configuration;
-        _restClient = new RestClient(_configuration["ConnectionStrings:ExchangeRatesConnection:Url"]!);
+        _restClient = new RestClient(_configuration.GetConnectionString("ExchangeRatesConnection:Url")!);//["ConnectionStrings:ExchangeRatesConnection:Url"]!);
     }
 
     public async Task<ExchangeRates> GetExchangeRates(string currency)
     {
         var request = new RestRequest("exchangerates/rates/c/" + currency + "/", Method.Get);
-        request.AddParameter("format", _configuration["ConnectionStrings:ExchangeRatesConnection:JsonFormat"]);
+        request.AddParameter("format", _configuration.GetConnectionString("ExchangeRatesConnection:JsonFormat"));
         var queryResult = await _restClient.ExecuteAsync(request);
         
         if (queryResult.Content == "404 NotFound - Not Found - Brak danych")
