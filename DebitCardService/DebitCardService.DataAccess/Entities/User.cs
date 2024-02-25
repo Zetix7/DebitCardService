5a7f5b615fa4af94dbc62160f51727461a014657
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-02-25 08:57:51 +0100

    Added properties to User class entity, Created new migration AddUserProperties

--- DebitCardService/DebitCardService.DataAccess/Entities/User.cs
+++ DebitCardService/DebitCardService.DataAccess/Entities/User.cs
@@ -4,6 +4,12 @@ namespace DebitCardService.DataAccess.Entities;
 
 public class User : EntityBase
 {
+    public enum Role
+    {
+        AdminService = 0,
+        UserService = 1
+    }
+
     public List<DebitCard>? DebitCards { get; set; }
 
     [StringLength(20, MinimumLength = 1)]
@@ -13,9 +19,15 @@ public class User : EntityBase
     public string? LastName { get; set; }
 
     [Required]
+    public Role AccessLevel { get; set; }
+
+    [Required]
     [StringLength(15, MinimumLength = 8)]
     public string? Login { get; set; }
     
     [Required]
     public string? Password { get; set; }
+
+    [Required]
+    public bool IsActive { get; set; }
 }
---
+++ DebitCardService/DebitCardService.DataAccess/Migrations/20240225075502_AddUserProperties.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DebitCardService.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccessLevel",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessLevel",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+        }
+    }
+}
