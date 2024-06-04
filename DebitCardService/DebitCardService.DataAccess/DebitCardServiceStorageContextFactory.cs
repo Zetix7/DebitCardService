fc9c27e2b7d1d365759fe7942d1d3c7be8f844ec
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-06-04 10:39:33 +0200

    Modified connetion to database in DebitCardServiceStorageContextFactory class

--- DebitCardService/DebitCardService.DataAccess/DebitCardServiceStorageContextFactory.cs
+++ DebitCardService/DebitCardService.DataAccess/DebitCardServiceStorageContextFactory.cs
@@ -8,8 +8,7 @@ public class DebitCardServiceStorageContextFactory : IDesignTimeDbContextFactory
     public DebitCardServiceStorageContext CreateDbContext(string[] args)
     {
         var optionBuilder = new DbContextOptionsBuilder<DebitCardServiceStorageContext>();
-        //optionBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=DebitCardServiceStorage;Integrated Security=True;Encrypt=False");
-        optionBuilder.UseSqlServer("Server=tcp:debit-card-service.database.windows.net,1433;Initial Catalog=DebitCardServiceStorage;Persist Security Info=False;User ID=greg;Password=I5e1S7EEKkY1NpMxuZUcfi;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=DebitCardServiceStorage;Integrated Security=True;Encrypt=False");
         return new DebitCardServiceStorageContext(optionBuilder.Options);
     }
 }
