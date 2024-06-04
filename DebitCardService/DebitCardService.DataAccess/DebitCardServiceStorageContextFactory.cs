1818a59a08ed7a83c34588429ecfcb58aa4fc5b3
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-06-04 10:38:08 +0200

    Clean solution

--- DebitCardService/DebitCardService.DataAccess/DebitCardServiceStorageContextFactory.cs
+++ DebitCardService/DebitCardService.DataAccess/DebitCardServiceStorageContextFactory.cs
@@ -8,7 +8,11 @@ public class DebitCardServiceStorageContextFactory : IDesignTimeDbContextFactory
     public DebitCardServiceStorageContext CreateDbContext(string[] args)
     {
         var optionBuilder = new DbContextOptionsBuilder<DebitCardServiceStorageContext>();
-        optionBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=DebitCardServiceStorage;Integrated Security=True;Encrypt=False");
+        //optionBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=DebitCardServiceStorage;Integrated Security=True;Encrypt=False");
+        optionBuilder.UseSqlServer("Server=tcp:debit-card-service.database.windows.net,1433;Initial Catalog=DebitCardServiceStorage;Persist Security Info=False;User ID=greg;Password=I5e1S7EEKkY1NpMxuZUcfi;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         return new DebitCardServiceStorageContext(optionBuilder.Options);
     }
 }
+
+// I5e1S7EEKkY1NpMxuZUcfi
+// Server=tcp:debit-card-service.database.windows.net,1433;Initial Catalog=DebitCardServiceStorage;Persist Security Info=False;User ID=greg;Password=I5e1S7EEKkY1NpMxuZUcfi;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
